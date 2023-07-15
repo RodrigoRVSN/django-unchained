fab2a01492ee81174ceb74ab71779a7ded49fa86
rodrigorvsn <rodrigovictor81@gmail.com>
2023-07-14 22:53:40 -0300

    feat: add a new migration to test creating a new migration

--- core/models.py
+++ core/models.py
@@ -4,3 +4,7 @@ class Products (models.Model):
   name = models.CharField('Name', max_length=100)
   price = models.DecimalField('Price', decimal_places=2, max_digits=9)
   stock = models.IntegerField('Quantity in stock')
+
+class Client (models.Model):
+  name = models.CharField('Name', max_length=100)
+  surname = models.CharField('Surname', max_length=100)
