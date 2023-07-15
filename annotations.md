4d2c3c7f5ac5cac6409b409239cdcd8c8a61a800
rodrigorvsn <rodrigovictor81@gmail.com>
2023-07-14 22:50:58 -0300

    feat: create example of Model with migrations

--- annotations.md
+++ annotations.md
@@ -9,3 +9,8 @@ from django.shortcuts import render
 def index(request):
   return render(request, 'index.html')
 ```
+
+
+# Flow
+
+- urls -> views -> templates
--- core/models.py
+++ core/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Products (models.Model):
+  name = models.CharField('Name', max_length=100)
+  price = models.DecimalField('Price', decimal_places=2, max_digits=9)
+  stock = models.IntegerField('Quantity in stock')
